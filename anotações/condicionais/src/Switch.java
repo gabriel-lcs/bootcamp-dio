098b7a5d75c3965cd64fe2d057f96907542abbee
Gabriel Lucas <gabriellucassr@hotmail.com>
2025-08-27 16:43:16 -0300

    adição da aula de switch case

---
+++ anotações/condicionais/src/Switch.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número de 1 a 7: ");
+        int option = sc.nextInt();
+
+        //Opção antiga
+        switch (option){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida, digite um número de 1 a 7.");
+        }
+
+        //Opção moderna
+        switch (option){
+            case 1, 7 -> System.out.println("Fim de semana.");
+            case 2 -> System.out.println("Segunda");
+            case 3 -> {
+                //Caso eu queira colocar mais coisas
+                System.out.println("Terça");
+            }
+            case 4 -> {
+                //Caso eu queira colocar mais coisas
+                System.out.println("Quarta");
+            }
+            case 5 -> System.out.println("Quinta");
+            case 6 -> System.out.println("Sexta");
+            default -> System.out.println("Opção inválida");
+        }
+
+    }
+
+}
