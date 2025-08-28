2159c69e5cebd119cc858fc339ee8ebb08e8f329
Gabriel Lucas <gabriellucassr@hotmail.com>
2025-08-28 11:16:47 -0300

    add laço enquanto

--- exemplos/lacos/src/enquanto.java
+++ exemplos/lacos/src/enquanto.java
@@ -1,5 +1,21 @@
+import javax.swing.plaf.nimbus.AbstractRegionPainter;
+import java.util.Scanner;
+
 public class enquanto {
     public static void main(String[] args) {
-        System.out.println("Ola mundo");
+        Scanner scanner = new Scanner(System.in);
+
+        String name = " ";
+        while (true){
+            System.out.println("Informe um nome: ");
+            name = scanner.nextLine();
+
+            if (name.equalsIgnoreCase("Exit")){
+                break;
+            }
+
+            System.out.println(name);
+        }
+
     }
 }
